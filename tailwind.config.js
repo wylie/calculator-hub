ec15d04efeba26f902b5677ede34782468c03f81
Wylie Fisher <me@wyliefisher.com>
2026-02-25 23:32:08 -0500

    Add Tailwind dark mode and dark styling to layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{astro,js,ts,jsx,tsx}",
   ],
+  darkMode: 'media', // Enable dark mode based on system preference
   theme: {
     extend: {
       fontFamily: {
